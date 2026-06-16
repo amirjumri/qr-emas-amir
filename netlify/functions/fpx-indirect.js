4cf1ac267d721497f0cab74bfc305c5f287f2d4f
Muhammad Amir <amirjumri@gmail.com>
2026-06-16 11:28:01 +0800

    Handle FPX indirect POST response

--- netlify/functions/fpx-indirect.js
+++ netlify/functions/fpx-indirect.js
@@ -1,8 +1,21 @@
-exports.handler = async function () {
+const querystring = require("querystring");
+
+exports.handler = async function(event){
+
+  let params = {};
+
+  if(event.httpMethod === "POST"){
+    params = querystring.parse(event.body || "");
+  }else{
+    params = event.queryStringParameters || {};
+  }
+
+  const qs = new URLSearchParams(params).toString();
+
   return {
     statusCode: 302,
     headers: {
-      Location: "/fpx/indirect.html"
+      Location: "/fpx/indirect.html?" + qs
     },
     body: ""
   };
